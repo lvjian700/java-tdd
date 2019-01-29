0416a90acd8cf00716bebe891bc506ab8e34e5a0
Jian Lv <jian.lv@rea-group.com>
2019-01-29 16:14:11 +0800

    Show not found screen

--- src/main/java/com/lv/view/SearchResultView.java
+++ src/main/java/com/lv/view/SearchResultView.java
@@ -2,11 +2,20 @@ package com.lv.view;
 
 import com.lv.repository.SearchResultRepository;
 
+import java.util.List;
+
 public class SearchResultView {
+    private final SearchResultRepository repository;
+
     public SearchResultView(SearchResultRepository repository) {
+        this.repository = repository;
     }
 
     public String load() {
+        List searchResults = repository.list();
+        if (searchResults.isEmpty()) {
+            return "SRS - Listing not found";
+        }
         return "SRS - show listings";
     }
 }
